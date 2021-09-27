835382ec2ed7e2d8561816590afc04a53c366530
cjuniordev <carlosrogeriocr02@gmail.com>
2021-09-27 14:10:50 -0300

    comments

--- mEPs/mEP7/mEP7.py
+++ mEPs/mEP7/mEP7.py
@@ -1,11 +1,20 @@
-def recabeCandidatos(n, L=[''], i=1):
+def recebeCandidatos(n, L=[''], i=1):
+    """
+    Recebe um número inteiro 'n', faz uma recursão para receber os nomes dos candidatos na posição i.
+    Retorna uma lista com n+1 posições com o nome de todos candidatos.
+    """
     if i <= n:
         nome = input()
-        return recabeCandidatos(n, L + [nome], i + 1)
+        return recebeCandidatos(n, L + [nome], i + 1)
     else:
         return L
 
 def recebeVotos(v, votos, i=0):
+    """
+    Recebe um número inteiro 'v', faz uma recursão para receber os votos dos eleitores.
+    cada voto para um c canditado, é somado 1 em sua posição na lista votos.
+    retorna uma lista com todos os votos, onde a posição 0 são os votos brancos, e a última posição para nulos.
+    """
     if i < v:
         voto = int(input())
         if voto >= (len(votos)-1):
@@ -17,6 +26,9 @@ def recebeVotos(v, votos, i=0):
         return votos
 
 def relatorio(candidatos, votos, i=1):
+    """
+    Imprime um relatório com os votos por candidato, votos nulos e brancos
+    """
     if i < len(candidatos):
         print(f'{candidatos[i]}: {votos[i]}')
         return relatorio(candidatos, votos, i + 1)
@@ -25,6 +37,9 @@ def relatorio(candidatos, votos, i=1):
         print(f'Nulos: {votos[len(votos)-1]}')
 
 def vencedor(candidatos, votos, candidato=0, maisVotado=0, i=0):
+    """
+    Verifica quem mais recebeu votos e retorna a posição do vencedor
+    """
     if i < len(votos):
         if votos[i] > maisVotado:
             return vencedor(candidatos, votos, i, votos[i], i+1)
@@ -34,12 +49,16 @@ def vencedor(candidatos, votos, candidato=0, maisVotado=0, i=0):
 
 def main():
     # numero de candidatos
-    n = int(input()) # 2 <= n <= 100
-    candidatos = recabeCandidatos(n)
+    n = int(input())
+    candidatos = recebeCandidatos(n)
 
     # numero de eleitores/votos
     v = int(input())
-    votos = recebeVotos(v, [0]*(n+2))
+    
+    # lista com tamanho n+2, pois n ==  numero de candidatos,
+    # posição 0 para voto branco e posição n+1 para nulos
+    l = [0]*(n+2)
+    votos = recebeVotos(v, l)
 
     relatorio(candidatos, votos)
     
